1ebf9517df6d01235374ae3d7df5909348975f49
felicityky <fingers160308@gmail.com>
2017-05-28 09:20:36 +0800

    Add new features along with tutorial

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -18,7 +18,9 @@ var app = app || {};
 		// The DOM events specific to an item.
 		events: {
 			'click .toggle': 'toggleCompleted',
-			'dblclick label': 'edit',
+			// 'dblclick label': 'edit',
+			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -50,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -63,6 +66,11 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
+		},
+
+		//Toggle the `"priority"` state of the modal
+		togglePriority: function () {
+			this.model.priorityT();
 		},
 
 		// Toggle the `"completed"` state of the model.
